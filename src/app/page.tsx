47664765d65891b98b03c6cfa7e684a8985f729d
Hector G <nomad.tikal@gmail.com>
2026-04-30 04:08:57 -0700

    fix: Final rendering order and layout stability refinements for 90+ Lighthouse score

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import AgencyHero from "@/components/AgencyHero";
 import HomeClientSections from "@/components/HomeClientSections";
 import PageClientOverlays from "@/components/PageClientOverlays";
+import PageMiddleSections from "@/components/PageMiddleSections";
 
 export default function Home() {
   return (
     <main className="bg-black font-inter relative">
       <PageClientOverlays />
       <AgencyHero />
+      <PageMiddleSections />
       <HomeClientSections />
     </main>
   );
 }
+
 
 
 
